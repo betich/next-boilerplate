a88613aaf49c2ded3729d88c2c24fa6e22c78074
betich <betich365@gmail.com>
2022-07-15 18:07:34 +0700

    misc: misc fixes on multiple files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,12 +11,6 @@ export default class Document extends NextDocument {
       <Html lang="th">
         <Head>
           <meta charSet="utf-8" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&family=Noto+Sans+Thai:wght@300;400;600&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
