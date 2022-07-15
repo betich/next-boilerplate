f424f686c9c32eac5a305d4e6958d791fc51d37d
betich <betich365@gmail.com>
2022-07-15 20:01:40 +0700

    refactor: add alias paths

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack(config, options) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@styles": join(__dirname, "src/styles"),
+      "@components": join(__dirname, "src/components"),
+      "@pages": join(__dirname, "src/pages"),
+      "@helpers": join(__dirname, "src/helpers"),
+      "@types": join(__dirname, "src/types"),
+      "@types": join(__dirname, "src/types"),
+      "@modules": join(__dirname, "src/modules"),
+      "@utils": join(__dirname, "src/utils"),
+      "@utilities": join(__dirname, "src/utilities"),
+      "@handlers": join(__dirname, "src/handlers"),
+      "@config": join(__dirname, "src/config"),
+      "@models": join(__dirname, "src/models"),
+      "@hooks": join(__dirname, "src/hooks"),
+      "@lib": join(__dirname, "src/lib"),
+      "@services": join(__dirname, "src/services"),
+      "@vectors": join(__dirname, "src/vectors"),
+    }
+
+    return config
+  },
 }
