2b3173e89341da8a96b68087af98dfcb3ad7ef06
betich <betich365@gmail.com>
2022-11-24 04:42:20 +0700

    fix: remove pwa field in next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
   dest: "public",
+  swSrc: "service-worker.js",
 })
 const { join } = require("path")
 
 module.exports = withPWA({
   reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    swSrc: "service-worker.js",
-  },
   webpack(config, options) {
     config.resolve.alias = {
       ...config.resolve.alias,
